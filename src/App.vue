b00313b3864297b699e4accbd3ae77e86f0c2288
kant0 <1195510518@qq.com>
2020-11-06 18:03:53 +0800

    money页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-  <router-view />
+    <router-view />
   </div>
 </template>
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
-<template>
-  <div>
-    Money
+<template >
+  <div class="nav-wrapper">
+    <div class="content">Money</div>
     <Nav />
   </div>
 </template>
@@ -10,4 +10,16 @@ export default {
   name: "Money",
 };
 </script>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
+</style>
 
