97dc7ecc1c32bde3ba9c7098c3bf08811ec3cd2c
kant0 <1195510518@qq.com>
2020-11-08 19:41:49 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <template>
-  <div>
+  <div id="app">
     <router-view />
   </div>
 </template>
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
+
 body {
   line-height: 1.5;
-}
-a {
-  text-decoration: none;
-  color: inherit;
+  font-family: $font-hei;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #333;
 }
 </style>
